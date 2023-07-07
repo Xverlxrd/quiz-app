62a80d1906920213479a5bac4615c729caf9f64c
Xverlxrd <tur-2002@mail.ru>
2023-07-07 15:00:10 +0700

    Настроил вебпак

---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebPackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = process.env.NODE_ENV === 'production'
+const babelOptions = preset => {
+    const opts = {
+        presets: [
+            '@babel/preset-env'
+        ]
+    }
+
+    if (preset){
+        opts.presets.push(preset)
+    }
+    return opts
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: "development",
+    entry: {
+        main: ['@babel/polyfill', './index.tsx'],
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.png', '.jsx', '.ts', '.tsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
+    devServer: {
+        port: 1337,
+        hot: isDev
+    },
+
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: "quiz-app",
+            template: path.resolve(__dirname, 'public/index.html'),
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].js'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ {
+                    loader: MiniCssExtractPlugin.loader,
+                }, 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/, //Для поддержки картинок и импортов
+                use: [ 'file-loader' ]
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: [ 'file-loader' ]
+            },
+            {
+                test: /\.xml$/,
+                use: [ 'xml-loader' ]
+            },
+            {
+                test: /\.csv$/,
+                use: [ 'csv-loader' ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: babelOptions()
+                }
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: babelOptions( '@babel/preset-typescript')
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: babelOptions('@babel/preset-react')
+                }
+            },
+        ]
+    }
+}
